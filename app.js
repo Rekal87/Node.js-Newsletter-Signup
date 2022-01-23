bcb717f4123ccd94678bffbf1aeaa1cd47c74626
Nnamdi <nnamdi.b@gmail.com>
2022-01-23 02:35:55 +0100

    added a new API key for the authentication

--- app.js
+++ app.js
@@ -2,24 +2,26 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
-const https = require("https")
+const https = require("https");
 const port = 3000;
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server is running on port:3000")
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port:3000");
 });
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/signup.html")
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
 app.use(express.static("public"));
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
   const userName = req.body.fName;
   const userSurname = req.body.lName;
   const userMail = req.body.email;
@@ -27,14 +29,16 @@ app.post("/", function(req, res) {
   console.log(`name: ${userName} lastname: ${userSurname} email: ${userMail}`);
 
   const data = {
-    members: [{
-      email_address: userMail,
-      status: "subscribed",
-      merge_fields: {
-        FNAME: userName,
-        LNAME: userSurname
-      }
-    }]
+    members: [
+      {
+        email_address: userMail,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: userName,
+          LNAME: userSurname,
+        },
+      },
+    ],
   };
 
   const jsonData = JSON.stringify(data);
@@ -43,27 +47,25 @@ app.post("/", function(req, res) {
 
   const options = {
     method: "POST",
-    auth: "rekal:3528cc2af08995591ea6ac0b2289c683-us20"
-  }
+    auth: "rekal:4153b83ca6b61b7aceb0d3c4ee6d76ee-us20",
+  };
 
-  const request = https.request(url, options, function(response) {
-
-
-    response.on("data", function(data) {
+  const request = https.request(url, options, function (response) {
+    response.on("data", function (data) {
       console.log("Status code: " + response.statusCode);
     });
 
     if (response.statusCode === 200) {
-      res.sendFile(__dirname + "/success.html")
+      res.sendFile(__dirname + "/success.html");
     } else {
       res.sendFile(__dirname + "/failure.html");
-    };
+    }
   });
 
   request.write(jsonData);
   request.end();
 });
 
-app.post("/failure.html", function(req, res) {
+app.post("/failure.html", function (req, res) {
   res.redirect("/");
-})
+});
